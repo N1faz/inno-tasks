32da7740781d8dbda1b7c829b228357b51e96bc5
Vladislav <vlad20042010v@gmail.com>
2026-07-06 18:33:15 +0300

    Решение по модулю 13 (основано на задании по модулю 12) находится в директории module12

--- module12/app.py
+++ module12/app.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+import os
 from flask import Flask
 from ping3 import ping
+import psycopg2
 
 app = Flask(__name__)
+
+# Подключение к БД
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 @app.route('/')
 def ping_google():
@@ -14,6 +19,14 @@ def ping_google():
             return "Пинг google.com не удался (нет ответа)!!!"
     except Exception as e:
         return f"Ошибка: {e}!!!"
+
+@app.route('/db')
+def check_db():
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        return "Подключение к PostgreSQL успешно!"
+    except Exception as e:
+        return f"Ошибка подключения к БД: {e}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
