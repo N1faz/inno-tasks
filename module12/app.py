a4bf53381702f150a41de8d145ad10a2ee63c6c4
Vladislav <vlad20042010v@gmail.com>
2026-07-09 00:50:59 +0300

    Добавил решение на модуль 15 в файл module 12 (Observability (Prometheus и Grafana))

--- module12/app.py
+++ module12/app.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 import os
-from flask import Flask
+from flask import Flask, Response
 from ping3 import ping
 import psycopg2
+from prometheus_client import Counter, generate_latest, REGISTRY
 
 app = Flask(__name__)
 
-# Подключение к БД
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Создаем счетчик запросов
+REQUESTS = Counter('http_requests_total', 'Total HTTP requests', ['endpoint'])
 
 @app.route('/')
 def ping_google():
+    REQUESTS.labels(endpoint='/').inc()
     try:
         response = ping("google.com", timeout=3)
         if response is not None:
@@ -22,6 +26,7 @@ def ping_google():
 
 @app.route('/db')
 def check_db():
+    REQUESTS.labels(endpoint='/db').inc()
     try:
         conn = psycopg2.connect(DATABASE_URL)
         return "Подключение к PostgreSQL успешно!"
@@ -30,7 +35,12 @@ def check_db():
 
 @app.route('/app')
 def app_route():
+    REQUESTS.labels(endpoint='/app').inc()
     return "Добро пожаловать на /app!"
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(REGISTRY), mimetype='text/plain')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
